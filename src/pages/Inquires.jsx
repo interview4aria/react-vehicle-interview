10f4e16e375a4906720aae11cd3069d64d31f8b9
Reginald Gabriel <rgabriel@ariasystems.com>
2022-02-04 14:32:48 -0500

    remove unused imports and remove unused constructor from Inquires component

--- src/pages/Inquires.jsx
+++ src/pages/Inquires.jsx
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
 
 export default class Inquires extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
       <div className="container" style={{marginTop: '40px'}}>
